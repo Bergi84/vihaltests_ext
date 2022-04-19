3edc7d89af178f4691d9ac1165ac0d5fe3a9dfa0
Bergi84 <Klaus.Bergkirchner@gmail.com>
2022-04-19 18:13:37 +0200

    - zigbee initialization is working - zigbee trace from M0 implemented - zigbee transport layer debugging implemented - RF wake up clock selection added to hwclktree

--- zigbee/src/main.cpp
+++ zigbee/src/main.cpp
@@ -18,18 +18,16 @@
 #include "timeServer.h"
 #include "board_pins.h"
 #include "zigbee_stm32wb.h"
+#include "sequencer_armm.h"
 
 THwRtc gRtc;
 THwClkTree gClkTree;
 TTimerServer gTs;
 TLowPowerManger gLpm;
 THwPwr gPwr;
-extern "C" void IRQ_Handler_03() {gTs.irqHandler();}
+TSequencer gSeq;
 
-void timerHandler0(THwRtc::time_t aTime);
-void timerHandler1(THwRtc::time_t aTime);
-void timerHandler2(THwRtc::time_t aTime);
-void timerHandler3(THwRtc::time_t aTime);
+extern "C" void IRQ_Handler_03() {gTs.irqHandler();}
 
 void setPowerMode(TLowPowerManger::lpm_t aLpm);
 
@@ -59,20 +57,25 @@ extern "C" __attribute__((noreturn)) void _start(unsigned self_flashing)  // sel
 
 	gPwr.init();
 
+	// setup clock configuration for maximal speed
 	gClkTree.init();
 	gClkTree.loadHseTune();
 	gClkTree.setRtcClkSource(THwClkTree::RTC_LSE);
-	gClkTree.confFlashForSpeed(THwClkTree::hseSpeed*2);
+	gClkTree.confFlashForSpeed(THwClkTree::hseSpeed);
 	gClkTree.setPllClkSource(THwClkTree::PLL_HSE, 8);
 	gClkTree.confPllMain(64, 4, 4, 4);
+	gClkTree.setCpu2ClkDiv(2);
+	gClkTree.setFlashClkDiv(2);
 	gClkTree.setSysClkSource(THwClkTree::SYS_PLLRCLK);
-	gClkTree.setMsiSpeed(2000000);
 	gClkTree.setSysStopClkSource(THwClkTree::SYS_HSI16);
+	gClkTree.setRfWakeupClkSource(THwClkTree::RFWU_LSE);
 
-	gClkTree.setOutClkSource(THwClkTree::OUT_HSE, 1);
+	// clock output for debugging
+	gClkTree.setOutClkSource(THwClkTree::OUT_SYSCLK, 1);
 
+	// enable and configure SMPS
 	gClkTree.setSmpsClkSource(THwClkTree::SMPS_HSI16, true);
-	gPwr.enableSMPS(1750, 220);
+	//gPwr.enableSMPS(1750, 220);
 
 	clockcnt_init();
 
@@ -91,78 +94,29 @@ extern "C" __attribute__((noreturn)) void _start(unsigned self_flashing)  // sel
 	gLpm.registerApp(locLpmId);
 	gLpm.disableLpMode(locLpmId, TLowPowerManger::LPM_off);
 
-	gTrace.init(0, &gLpm);
+	gSeq.init();
 
-	THwRtc::time_t startTime, lastTime;
-	startTime.msec = 768;
-	startTime.sec = 13;
-	startTime.min = 43;
-	startTime.hour = 13;
-	startTime.day = 13;
-	startTime.month = 03;
-	startTime.year = 22;
-	gRtc.setTime(startTime, lastTime);
-
-	printTime();
+	gTrace.init(0, &gLpm, &gSeq);
 
 	TRACE("\r\n--------------------------------------\r\n");
-	TRACE("%sHello From VIHAL !\r\n", CC_BLU);
+	TRACE("Hello From VIHAL !\r\n");
 	TRACE("Board: %s\r\n", BOARD_NAME);
-	TRACE("SystemCoreClock: %u\r\n", SystemCoreClock);
-
-	printTime();
+  uint32_t sysSpeed;
+  gClkTree.getSysClkSpeed(sysSpeed);
+	TRACE("SystemCoreClock: %u\r\n", sysSpeed);
 
 	// init time server
 	uint8_t TimerID[4];
 
 	gTs.init(&gRtc);
 
-	gTs.create(TimerID[0], timerHandler0, true);
-	gTs.create(TimerID[1], timerHandler1, true);
-	gTs.create(TimerID[2], timerHandler2, true);
-	gTs.create(TimerID[3], timerHandler3, true);
-	gTs.start(TimerID[0], 10000);
-	gTs.start(TimerID[1], 1000);
-	gTs.start(TimerID[2], 500);
-	gTs.start(TimerID[3], 5000);
-	printTime();
-
 	mcu_enable_interrupts();
 
 	gLpm.disableLpMode(locLpmId, TLowPowerManger::LPM_Run);
 
-  gZigbee.init();
+  gZigbee.init(&gSeq, &gPwr);
 
-	// Infinite loop
-	while (1)
-	{
-	  gTrace.service();
-	  gLpm.enterLowPowerMode();
-	}
-}
-
-void timerHandler0(THwRtc::time_t aTime)
-{
-  TRACE("%sTimer0: ", CC_RED);
-  TRACE("%02hhu:%02hhu:%02hhu.%03hu %02hhu.%02hhu.%02hhu\r\n", aTime.hour, aTime.min, aTime.sec, aTime.msec, aTime.day, aTime.month, aTime.year);
-}
-
-void timerHandler1(THwRtc::time_t aTime)
-{
-  TRACE("%sTimer1: ", CC_MAG);
-  TRACE("%02hhu:%02hhu:%02hhu.%03hu %02hhu.%02hhu.%02hhu\r\n", aTime.hour, aTime.min, aTime.sec, aTime.msec, aTime.day, aTime.month, aTime.year);
-}
-
-void timerHandler2(THwRtc::time_t aTime)
-{
-  TRACE("%sTimer2: ", CC_YEL);
-  TRACE("%02hhu:%02hhu:%02hhu.%03hu %02hhu.%02hhu.%02hhu\r\n", aTime.hour, aTime.min, aTime.sec, aTime.msec, aTime.day, aTime.month, aTime.year);
-}
-
-void timerHandler3(THwRtc::time_t aTime)
-{
-  TRACE("%sTimer3: ", CC_BLU);
-  TRACE("%02hhu:%02hhu:%02hhu.%03hu %02hhu.%02hhu.%02hhu\r\n", aTime.hour, aTime.min, aTime.sec, aTime.msec, aTime.day, aTime.month, aTime.year);
+  gSeq.startIdle();
 }
 
 void setPowerMode(TLowPowerManger::lpm_t aLpm)
--- zigbee/src/test.cpp
+++
@@ -1,6 +0,0 @@
-#include "zigbee_interface.h"
-
-void test()
-{
-  ZbInit(0U, NULL, NULL);
-}
